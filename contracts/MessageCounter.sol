25471afa459d1b215da1900e144e63fbdef4fd13
Tonber Arch <rupejo5@gmail.com>
2026-03-15 15:26:30 +0100

    feat: add MessageCounter smart contract

---
+++ contracts/MessageCounter.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+contract MessageCounter {
+    uint256 private count;
+
+    event CounterIncremented(address indexed by, uint256 newCount);
+
+    function increment() public {
+        count += 1;
+        emit CounterIncremented(msg.sender, count);
+    }
+
+    function getCount() public view returns (uint256) {
+        return count;
+    }
+}
