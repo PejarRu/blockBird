b43796f83aed59067ac16570046469929512fdaa
Tonber Arch <rupejo5@gmail.com>
2026-03-15 16:32:32 +0100

    chore: clean deploy script output

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,20 +1,20 @@
 async function main() {
   const [deployer] = await ethers.getSigners();
 
-  console.log('Deploying contracts with account:', deployer.address);
+  console.log('=== BlockBird deployment ===');
+  console.log('Deployer:', deployer.address);
 
   const MessageCounter = await ethers.getContractFactory('MessageCounter');
   const counter = await MessageCounter.deploy();
   await counter.deployed();
-  console.log('MessageCounter deployed to:', counter.address);
+  console.log('1) MessageCounter:', counter.address);
 
   const BlockBird = await ethers.getContractFactory('BlockBird');
   const blockBird = await BlockBird.deploy(counter.address);
   await blockBird.deployed();
+  console.log('2) BlockBird:', blockBird.address);
 
-  console.log('BlockBird deployed to:', blockBird.address);
-
-  console.log('Integration: counter at', counter.address, 'used by BlockBird at', blockBird.address);
+  console.log('Integration ready: BlockBird -> MessageCounter');
 }
 
 main().catch((error) => {
