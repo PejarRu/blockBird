1fbbf1e3eed63f48f2a760a0dae053de498a83a3
Tonber Arch <rupejo5@gmail.com>
2026-03-15 15:26:30 +0100

    chore: add Hardhat config and npm scripts

---
+++ hardhat.config.js
@@ -0,0 +1,11 @@
+require('@nomiclabs/hardhat-ethers');
+
+module.exports = {
+  solidity: '0.8.9',
+  paths: {
+    sources: './contracts',
+    tests: './test',
+    cache: './cache',
+    artifacts: './artifacts'
+  }
+};
